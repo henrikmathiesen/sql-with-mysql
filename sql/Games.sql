134423832165af4090bdb23d55d9da9b36747c6a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-11 09:43:51 +0200

    -added columns developer and releaseyear

--- sql/Games.sql
+++ sql/Games.sql
@@ -3,7 +3,9 @@
 create table Games(
 	Id int primary key auto_increment,
     Name varchar(60) not null,
+    Developer varchar(60) not null,
     Publisher varchar(60) not null,
+    ReleaseYear int not null,
     AvarageRating int not null,
     UserId int not null,
     foreign key (UserId) references Users(Id)
