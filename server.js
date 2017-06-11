acbbec72c072297fc968dab535f841bea65a7a91
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-11 13:05:12 +0200

    -renamed connection file -set up a basic connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const isProduction = require('./server/isProduction.js');
 const express = require('express');
+const mySql = require('mysql');
+const isProduction = require('./server/isProduction.js');
+const dbConnection = require('./server/dbConnection.js');
+
 const app = express();
+const connection = mySql.createConnection(dbConnection);
+
+connection.connect();
+connection.end();
 
 app.listen(1337, function () {
   console.log('Listening on port 1337, production mode is: ' + isProduction);
