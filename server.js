7f41ea0d1e969e401c07eccdd1bbe3dec306537f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-11 13:09:05 +0200

    -working test connection

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const app = express();
 const connection = mySql.createConnection(dbConnection);
 
 connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', JSON.stringify(results));
+});
+
 connection.end();
 
-app.listen(1337, function () {
+app.listen(1337, () => {
   console.log('Listening on port 1337, production mode is: ' + isProduction);
 });
