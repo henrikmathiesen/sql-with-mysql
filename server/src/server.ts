49da4adf98dbdbe669f112dcdb048eb56f150179
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-18 08:07:46 +0200

    -removed uneccessary line -commented out seed

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,9 @@ import SeedUsers from './seedDb/SeedUsers';
 
 const app = express();
 
-if(!environment.isProduction) {
-    new SeedUsers().seed();
-}
+// if(!environment.isProduction) {
+//     new SeedUsers().seed();
+// }
 
 app.listen('1337', () => {
     console.log('Listening on port 1337, production mode is: ' + environment.isProduction);
