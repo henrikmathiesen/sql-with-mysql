22d439d561f46880fe205d52a318346f6c41d525
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-23 14:20:02 +0200

    -seed users, work in progress

---
+++ server/src/seedDb/seedUsers.ts
@@ -0,0 +1,35 @@
+import { UserDbo } from '../db/dbo/UserDbo';
+import { createUserQuery } from '../db/queries/users/createUserQuery';
+
+export const seed = () => {
+    const users = new Array<UserDbo>();
+
+    const user01 = new UserDbo();
+    user01.name = 'Adam Adamsson';
+    user01.email = 'adam@adamsson.foo';
+    user01.registered = new Date().toISOString();
+
+    const user02 = new UserDbo();
+    user02.name = 'Bertil Bertilsson';
+    user02.email = 'bertil@bertilsson.foo';
+    user02.registered = new Date().toISOString();
+
+    const user03 = new UserDbo();
+    user03.name = 'John Doe';
+    user03.email = 'john@doe.foo';
+    user03.registered = new Date().toISOString();
+
+    const user04 = new UserDbo();
+    user04.name = 'Jane Poe';
+    user04.email = 'jane@poe.foo';
+    user04.registered = new Date().toISOString();
+
+    users.push(user01);
+    users.push(user02);
+    users.push(user03);
+    users.push(user04);
+
+    // for (var user = 0; user < users.length; user++) {
+    //     createUserQuery(users[user], () => { });
+    // }
+};
