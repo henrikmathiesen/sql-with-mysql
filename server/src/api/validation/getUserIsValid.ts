781f8f9ef1580fa8fc04f9d43cc856615b95a815
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-25 11:20:20 +0200

    -validate empty strings

--- server/src/api/validation/getUserIsValid.ts
+++ server/src/api/validation/getUserIsValid.ts
@@ -7,9 +7,14 @@ export const getUserIsValid = (user: UserDbo) => {
     const nameIsAString = typeof user.name === 'string';
     const emailIsAString = typeof user.email === 'string';
     const registeredIsAnIso8601Date = moment(user.registered, moment.ISO_8601, true).isValid();
+
+    const nameIsNotEmpty = user.name;
+    const emailIsNotEmpty = user.email;
     
 
     return nameIsAString &&
         emailIsAString &&
-        registeredIsAnIso8601Date;
+        registeredIsAnIso8601Date &&
+        nameIsNotEmpty &&
+        emailIsNotEmpty;
 };
