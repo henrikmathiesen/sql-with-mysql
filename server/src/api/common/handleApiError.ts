db8e4bc3e28756d7e67629d5b30672789248e770
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-04 09:56:51 +0200

    -string concatination works

--- server/src/api/common/handleApiError.ts
+++ server/src/api/common/handleApiError.ts
@@ -1,6 +1,6 @@
 export const handleApiError = (req, res, error, showErrorDetailsToUser:boolean = false) => { 
     console.log(error);
     
-    const userErrorMessage = `Error in route: [${req.method}]${req.path}` + showErrorDetailsToUser ? ` ${error}` : ``;
+    const userErrorMessage = (`Error in route: [${req.method}]${req.path}`) + (showErrorDetailsToUser ? ` ${error}` : ``);
     res.status(500).json(userErrorMessage);
 };
