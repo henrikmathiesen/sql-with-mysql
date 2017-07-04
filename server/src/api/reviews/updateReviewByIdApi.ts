b27cdf0bafe789351bd9bd7bd6dad4952200b823
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-04 07:04:21 +0200

    -update review api

--- server/src/api/reviews/updateReviewByIdApi.ts
+++ server/src/api/reviews/updateReviewByIdApi.ts
@@ -2,3 +2,52 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { ReviewDbo } from '../../db/dbo/ReviewDbo';
 import { handleApiError } from '../common/handleApiError';
+import { getReviewIsValid, reviewIsInvalidMessage } from '../validation/getReviewIsValid';
+import { getEntityExists, entityExistsInvalidMessage } from '../validation/getEntityExists';
+import { updateEntityByIdQuery } from '../../db/queries/updateEntityByIdQuery';
+import { DbTableEnum } from '../../db/common/getDbTableConstants';
+import { updatedReviewBodyToReviewMapping, IReviewBody } from '../mapping/reviewBodyToReviewMapping';
+import { calculateGameAvarageRatingBasedOnReviews } from '../calculation/calculateGameAvarageRatingBasedOnReviews';
+
+const router = express.Router();
+router.use(bodyParser.json());
+
+router.put('/api/review/:id', (req, res) => {
+    const id: number = parseInt(req.params.id);
+    const review: IReviewBody = req.body;
+
+    if (!getReviewIsValid(review)) {
+        handleApiError(req, res, reviewIsInvalidMessage);
+        return;
+    }
+
+    getEntityExists(DbTableEnum.reviews, id)
+        .then((reviewExists: boolean) => {
+            if (!reviewExists) {
+                handleApiError(req, res, entityExistsInvalidMessage);
+            }
+            else {
+                const updatedReview = updatedReviewBodyToReviewMapping(review);
+
+                updateEntityByIdQuery(DbTableEnum.reviews, updatedReview, id)
+                    .then(() => {
+                        calculateGameAvarageRatingBasedOnReviews(updatedReview.gameId)
+                            .then(() => {
+                                res.end();
+                            })
+                            .catch((error) => {
+                                handleApiError(req, res, error);
+                            });
+                    })
+                    .catch((error) => {
+                        handleApiError(req, res, error);
+                    });
+            }
+        })
+        .catch((error) => {
+            handleApiError(req, res, error);
+        });
+
+});
+
+const updateReviewByIdApi = router;
