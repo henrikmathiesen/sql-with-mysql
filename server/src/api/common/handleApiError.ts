7ed95657756f7771c59c0c089b7ff4871bedcc67
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-07 10:01:46 +0200

    -send no stack trace in production -send custom error in production -send both in development

--- server/src/api/common/handleApiError.ts
+++ server/src/api/common/handleApiError.ts
@@ -1,9 +1,14 @@
 import { statusCodeConstants } from '../common/statusCodeConstants';
 import { isProduction } from '../../environment';
 
-export const handleApiError = (req, res, error) => { 
+// In production mode, send no stacktrace but send custom error message. In development mode, send both.
+
+const getErrorIsCustomError = (error: any) => { 
+    return typeof error === 'string';
+};
+
+export const handleApiError = (req, res, error) => {
     const errorInRoute = `Error in route: [${req.method}]${req.path}`;
-    const userErrorMessage = isProduction ? errorInRoute : errorInRoute + '\n' + error.stack;
-    
+    const userErrorMessage = isProduction && !getErrorIsCustomError(error) ? errorInRoute : (errorInRoute + '\n') + (error.stack || error);
     res.status(statusCodeConstants.internalServerError).send(userErrorMessage);
 };
