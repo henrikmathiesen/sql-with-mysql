cc808fbe69b55561b2b455ab94954d73cea26fb4
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-09-16 14:27:55 +0200

    -comments

--- server/src/uncaughtExceptionListener.ts
+++ server/src/uncaughtExceptionListener.ts
@@ -1,5 +1,7 @@
 import { endDb } from './db/endDb';
 
+// https://nodejs.org/api/process.html#process_event_uncaughtexception
+
 export const uncaughtExceptionListener = (onEndCb) => {
     process.once('uncaughtException', (error) => {
         endDb(() => { 
